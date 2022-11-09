fb144680419f4395271f05e95775ad3bc259e0c7
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-11-09 15:52:08 +0530

    LOOPS(PYTHON)

---
+++ loops10.py
@@ -0,0 +1,7 @@
+# Write a Program to Calculate Power of a Number
+# using inbuilt pow() function by taking two inputs from
+# users as Base and exponent respectively
+base = int(input("Enter The Base Number : \n"))
+exponent = eval(input("Enter The Exponent Number : \n"))
+result = pow(base , exponent)
+print(result)
